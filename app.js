cda835b54b5e986b26c63688641c100cf7d7897e
enver1473 <enverkovacevic@hotmail.com>
2020-10-07 17:46:25 +0200

    fixed transition

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function bgChanger() {
-    if (this.scrollY > this.innerHeight) {
-        document.body.classList.add("bg-active");
-    }
-    else {
-        document.body.classList.remove("bg-active");
-    }
+  if (this.scrollY > this.innerHeight * (1 / 3)) {
+    document.body.classList.add('bg-active');
+    document.body.classList.remove('bg');
+  } else {
+    document.body.classList.remove('bg-active');
+    document.body.classList.add('bg');
+  }
 }
 
-window.addEventListener("scroll", bgChanger);
+window.addEventListener('scroll', bgChanger);
