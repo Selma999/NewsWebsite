fe5c870031a2c1ff73bc4a6ba02535a2113d45be
selma999 <selmakovacevic1@hotmail.com>
2021-02-02 00:23:55 +0100

    changes

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
-$(document).ready(function() {
-    $('.menu').on('click', function() {
-        $(this).toggleClass('change');
-        $('.top-nav').toggleClass('change');
-    });
+$(document).ready(function () {
+  $(".menu").on("click", function () {
+    $(this).toggleClass("change");
+    $(".top-nav").toggleClass("change");
+  });
 
-    $('.top-nav .nav-link').on('click', function() {
-        $('.menu').removeClass('change');
-        $('.top-nav').removeClass('change');
-    });
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu").removeClass("change");
+    $(".top-nav").removeClass("change");
+  });
 
-    $('nav a[href*="#"]').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 1000);
-    });
-    
-    $(window).scroll(function () {
-        if($(this).scrollTop() > 40) {
-            $("#up").fadeIn();
-        } else {
-            $("#up").fadeOut();
-        }
-    });
-    $('#up').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-    });
+  $('nav a[href*="#"]').on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      1000
+    );
+  });
 
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once: true
-    })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $("#up").fadeIn();
+    } else {
+      $("#up").fadeOut();
+    }
+  });
+  $("#up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true,
+  });
+});
+
+$("#submitbtn").on("click", function () {
+  const email = $("#email")[0].value;
+  const name = $("#name")[0].value;
+
+  console.log(email, name);
+
+  $.post("http://localhost:3000/contact", { email, name });
 });
